4414612067fbd1bb5b632457898d71ad07469008
TejasBTECHWAla <tdongare1@gmail.com>
2024-07-17 09:12:13 +0530

    initial setup complete

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <div>
+  <h1>Expense Tracker</h1>
+ </div>
+);
+
+
