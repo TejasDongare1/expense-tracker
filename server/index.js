b23467e85b21f13402d43d67a49231f93370881e
TejasBTECHWAla <tdongare1@gmail.com>
2024-07-17 17:19:12 +0530

    signup api added

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
-
 dotenv.config()
+
+import User from "./models/User.js"
+import Transaction from "./models/Transaction.js"
+
 
 const app = express();
 app.use(express.json());
@@ -26,6 +29,38 @@ app.get("/", (req, res)=>{
     res.json({
         message: "API is running"
     })
+})
+
+app.post("/signup", async(req, res)=>{
+    const {fullName , email, password, dob  } = req.body;
+
+    const user = new User({
+        fullName,
+        email,
+        password,
+        dob: new Date(dob) 
+    });
+
+    try{
+    const savedUser = await user.save();
+
+    res.json({
+        success: true,
+        message: "Signup successfully",
+        data: savedUser
+    })
+}
+catch(e){
+    res.json({
+            success: false,
+            message: e.message,
+            data: null
+        })
+    }
+})
+
+app.post("/login", (req, res)=>{
+
 })
 
 const PORT = process.env.PORT || 5000;
